1af23012add77713b9252df3a8df9ada19410bdc
Jonathan Durieu <j.durieu@gmail.com>
2016-02-12 08:36:21 +0100

    Initial Commit

---
+++ Sell'n Yum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sell'n Yum
+//
+//  Created by Jonathan Durieu on 12/02/16.
+//  Copyright © 2016 Jonathan Durieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
